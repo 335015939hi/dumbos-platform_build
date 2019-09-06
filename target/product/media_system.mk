13a93ebca99fd82679d4455a4778dc5a50056728
Daniel Micay <danielmicay@gmail.com>
2019-09-06 07:24:59 -0400

    include Updater app when OFFICIAL_BUILD=true

--- target/product/media_system.mk
+++ target/product/media_system.mk
@@ -40,6 +40,10 @@ ifeq ($(RELEASE_PACKAGE_COMPUTER_CONTROL),true)
   PRODUCT_PACKAGES += VirtualDeviceManager
 endif
 
+ifeq ($(OFFICIAL_BUILD),true)
+    PRODUCT_PACKAGES += Updater
+endif
+
 PRODUCT_HOST_PACKAGES += \
     fsck.f2fs \
 
