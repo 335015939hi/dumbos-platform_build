posativ/platform_build

set deny_new_usb feature to dynamic by default
Change-Id: Ied8e75e6c7f8cc5e1483fe93281a32fe799638c3

core/main.mk
@@ -317,6 +317,7 @@ ifneq (,$(user_variant))
   # Target is secure in user builds.
   ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=1
   ADDITIONAL_DEFAULT_PROPERTIES += security.perf_harden=1
+  ADDITIONAL_DEFAULT_PROPERTIES += persist.security.deny_new_usb=dynamic
 
   ifeq ($(user_variant),user)
     ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1

335015939hi/dumbos-platform_build

core/main.mk
@@ -391,6 +391,7 @@ ifneq (,$(user_variant))
   ADDITIONAL_SYSTEM_PROPERTIES += ro.secure=1
   ADDITIONAL_SYSTEM_PROPERTIES += security.perf_harden=1
   ADDITIONAL_SYSTEM_PROPERTIES += ro.control_privapp_permissions=enforce
+  ADDITIONAL_SYSTEM_PROPERTIES += persist.security.deny_new_usb=dynamic
 
   ifeq ($(user_variant),user)
     ADDITIONAL_SYSTEM_PROPERTIES += ro.adb.secure=1