e648f99cf652ff306477ea5fa2da10b5ac4bf976
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-11-22 12:47:08 +0200

    add NetworkLocation

--- target/product/handheld_system.mk
+++ target/product/handheld_system.mk
@@ -61,6 +61,7 @@ PRODUCT_PACKAGES += \
     MmsService \
     MtpService \
     MusicFX \
+    NetworkLocation \
     PacProcessor \
     preinstalled-packages-platform-handheld-system.xml \
     PrintRecommendationService \
